2839d10ee479163656035576ce851b7147f79a54
Pawel Bochenek <p.bochenek@mllabs.services>
2024-12-17 19:28:32 +1100

    Updated

--- DataSource1_1/DataSource1_1.cs
+++ DataSource1_1/DataSource1_1.cs
@@ -12,7 +12,7 @@ namespace DataSource1_1
 	{
 		public GQIColumn[] GetColumns()
 		{
-			// Define data source columns
+			// Define data source columns srcs
 			// See: https://aka.dataminer.services/igqidatasource-getcolumns
 			return Array.Empty<GQIColumn>();
 		}
